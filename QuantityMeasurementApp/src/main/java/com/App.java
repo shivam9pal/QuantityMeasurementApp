8e06601bd6916cb8716dff95efea8520809535c8
shivam9pal <shivampaul9794@gmail.com>
2026-02-18 22:38:53 +0530

    [Shivam Pal]Add . feature/UC1-FeetEquality

---
+++ QuantityMeasurementApp/src/main/java/com/App.java
@@ -0,0 +1,25 @@
+package com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Feet value1 = new Feet(1.0);
+        Feet value2 = new Feet(1.0);
+
+        boolean result = value1.equals(value2);
+
+        System.out.println("Input: 1.0 ft and 1.0 ft");
+        System.out.println("Output: Equal (" + result + ")");
+        
+        
+        
+
+        
+        
+    }
+}
